3e36f5f071707671a3bf70b9e838fe21363964c7
MarinaA2015 <m.abrashkina2015@gmail.com>
2019-09-26 20:55:09 +0300

    Asserts methods were added to classes "WebHomePageTests" and "WebLoginPageTests"

--- src/tests/WebHomePageTests.java
+++ src/tests/WebHomePageTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -15,7 +16,8 @@ public class WebHomePageTests extends TestBase{
         WebElement eventList = driver.findElement(By.id("idtitletypesearchevents"));
         System.out.println("List events element exists: " + eventList.getText().equals("List events"));
         WebElement loginIcon = driver.findElement(By.id("idsignin"));
-        System.out.println("Login Icon exists: " + loginIcon.getText().equals("Login"));
+        //System.out.println("Login Icon exists: " + loginIcon.getText().equals("Login"));
+        Assert.assertEquals(loginIcon.getText(), "Login");
 
     }
 }
